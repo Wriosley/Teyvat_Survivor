2741d39043434ffc275c2b53fd621003479fd2ef
Sharksbeer <799458100@qq.com>
2025-07-01 11:06:40 +0800

    更新敌人类

--- LessonDesign/main.cpp
+++ LessonDesign/main.cpp
@@ -6,9 +6,13 @@
 #include <string>
 using namespace std;
 
+const int WINDOW_WIDTH = 1280; // 窗口宽度
+const int WINDOW_HEIGHT = 720; // 窗口高度
+
 int idx_current_anim = 0; //当前动画的帧索引
 const int PLAYER_ANIM_NUM = 6; //玩家动画帧数
 const int PLAYER_SPEED = 5; // 玩家移动速度
+
 
 const int PLAYER_WIDTH = 80; // 玩家宽度
 const int PLAYER_HEIGHT = 80; // 玩家高度
@@ -78,10 +82,145 @@ private:
 	vector<IMAGE*> frame_list; // 存储动画帧>
 };
 
+
 class Bullet
 {
 public:
+	POINT position = { 0,0 };
 
+public:
+	Bullet() = default;
+	~Bullet() = default;
+
+	void Draw() const
+	{
+		setlinecolor(RGB(255, 155, 50));
+		setfillcolor(RGB(200, 75, 10));
+		fillcircle(position.x, position.y,RADIUS); // 绘制子弹
+	}
+private:
+	const int  RADIUS = 10; // 子弹半径
+};
+
+class Player
+{
+
+};
+
+Player player;
+
+class Enemy
+{
+public:
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("./img/shadow_enemy.png")); // 加载敌人阴影图片
+		anim_left = new Animation(_T("./img/enemy_left_%d.png"), 6, 45); // 敌人向左移动动画
+		anim_right = new Animation(_T("./img/enemy_right_%d.png"), 6, 45); // 敌人向右移动动画
+
+		enum class SpawnEdge
+		{
+			Up = 0, Down, Left, Right
+		};
+
+		SpawnEdge edge = static_cast<SpawnEdge>(rand() % 4); // 随机生成敌人出生边缘
+		switch (edge)
+		{
+		case SpawnEdge::Up: // 上边缘
+			position.x = rand() % WINDOW_WIDTH; // 随机生成x坐标
+			position.y = -FRAME_HEIGHT;
+			break;
+		case SpawnEdge::Down:
+			position.x = rand() % WINDOW_WIDTH; // 随机生成x坐标
+			position.y = WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Left: // 左边缘
+			position.x = -FRAME_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT; // 随机生成y坐标
+			break;
+		case SpawnEdge::Right: // 右边缘
+			position.x = WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT; // 随机生成y坐标
+			break;
+		default:
+			break;
+		}
+		
+
+
+	}
+
+	~Enemy()
+	{
+		delete anim_left; // 释放向左移动动画资源
+		delete anim_right; // 释放向右移动动画资源
+	}
+
+	bool CheckBulletCollision(const Bullet& bullet)
+	{
+		return false;
+	}
+
+	bool CheckBulletCollision(const Player& player)
+	{
+		return false;
+	}
+
+	void Move(const Player& player)
+	{
+		const POINT& player_position = player_pos; // 获取玩家位置
+		dir_x = player_position.x - position.x; // 计算水平方向
+		dir_y = player_position.y - position.y; // 计算垂直方向
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y); // 计算方向向量的长度
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir; // 归一化水平方向
+			double normalized_y = dir_y / len_dir; // 归一化垂直方向
+			position.x += static_cast<int>(normalized_x * SPEED); // 更新玩家位置
+			position.y += static_cast<int>(normalized_y * SPEED); // 更新玩家位置
+		}
+
+	}
+
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH - SHADOW_WIDTH) / 2-5; // 阴影位置计算
+		int pos_shadow_y = position.y + FRAME_HEIGHT -25; // 阴影位置计算
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow); // 绘制阴影
+
+		static bool facing_left; // 玩家是否面向左侧
+		if (dir_x < 0)
+		{
+			facing_left = true; // 向左移动
+		}
+		else if (dir_x > 0)
+		{
+			facing_left = false; // 向右移动
+		}
+		if (facing_left)
+		{
+			anim_left->Play(position.x, position.y, delta); // 播放向左移动动画
+		}
+		else
+		{
+			anim_right->Play(position.x, position.y, delta); // 播放向右移动动画
+		}
+		
+	}
+
+private:
+	int dir_x;
+	int dir_y;
+	const int SPEED = 2;
+	const int FRAME_WIDTH = 80;
+	const int FRAME_HEIGHT = 80; // 敌人动画帧高度
+	const int SHADOW_WIDTH = 32; 
+
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right; // 敌人向右移动动画
+	POINT position = { 0,0 }; // 敌人位置
+	bool facing_left = false;
 };
 
 
@@ -92,7 +231,8 @@ Animation anim_right_player(_T("./img/player_right_%d.png"), 6, 45); // 玩家向左
 void DrawPlayer(int delta,int dir_x)
 {
 	int pos_shadow_x = player_pos.x + (PLAYER_WIDTH - SHADOW_WIDTH) / 2; // 阴影位置计算
-	int pos_shadow_y = player_pos.y + PLAYER_HEIGHT - 8; // 阴影位置计算
+	int pos_shadow_y = player_pos.y + PLAYER_HEIGHT-8; // 阴影位置计算
+	putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
 
 	static bool facing_left; // 玩家是否面向左侧
 	if (dir_x<0)
@@ -111,7 +251,17 @@ void DrawPlayer(int delta,int dir_x)
 	{
 		anim_right_player.Play(player_pos.x, player_pos.y, delta); // 播放向右移动动画
 	}
-	putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+	
+}
+
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
+{
+	const int INTERVAL = 100;
+	static int counter = 0;
+	if ((++counter) % INTERVAL == 0) // 每隔一定帧数生成一个敌人
+	{
+		enemy_list.push_back(new Enemy()); // 将新敌人添加到列表中
+	}
 }
 
 int main()
@@ -127,6 +277,8 @@ int main()
 
 	ExMessage msg;
 	IMAGE img_background;
+	std::vector<Enemy*> enemy_list;
+
 
 	loadimage(&img_background, _T("./img/background.png"), 1280, 720);
 	//LoadAnimation();
@@ -137,6 +289,14 @@ int main()
 		DWORD start_time = GetTickCount();
 
 		BeginBatchDraw();
+
+		/********************
+
+			消息处理部分
+
+		*********************/
+
+
 		while (peekmessage(&msg))
 		{
 			if (msg.message == WM_KEYDOWN)
@@ -180,6 +340,15 @@ int main()
 			}
 		} 
 
+		/********************
+
+			数据处理部分
+
+		*********************/
+
+		TryGenerateEnemy(enemy_list); // 尝试生成敌人
+		for (Enemy* enemy : enemy_list) enemy->Move(player);
+
 		//if (is_move_up) player_pos.y -= PLAYER_SPEED;
 		//if (is_move_down) player_pos.y += PLAYER_SPEED;
 		//if (is_move_left) player_pos.x -= PLAYER_SPEED;
@@ -202,7 +371,13 @@ int main()
 		//循环动画播放
 		idx_current_anim %= PLAYER_ANIM_NUM; 
 
+	/********************
 	
+	      渲染画面部分
+	
+	*********************/
+		
+
 		DWORD end_time = GetTickCount();
 		
 		DWORD delta_time = end_time - start_time;
@@ -215,6 +390,7 @@ int main()
 		
 		DrawPlayer(delta_time, dir_x);
 
+		for (Enemy* enemy : enemy_list) enemy->Draw(1000 / 144); // 绘制所有敌人
 
 		FlushBatchDraw();
 
